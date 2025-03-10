550518caa8e1d50d46d1e94a0b6c27e7204e4034
yaseenasaad12 <yaseenasaad12@gmail.com>
2025-03-10 09:26:22 +0200

    Fix some Errors

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,10 @@ package main.java;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -24,23 +26,49 @@ public class Main {
                 System.out.println("4. Exit");
                 System.out.print("Choose an option: ");
                 int choice = scanner.nextInt();
+                while(choice > 4 || choice <1)
+                 {
+                    System.out.print("\u001B[31mPlease enter valid option (1,2,3,4): \u001B[0m");
+                    choice = scanner.nextInt() ;
+                 }
 
                 if (choice == 4)
                     break;
 
-                System.out.print("Return intermediate arrays? (true/false): ");
-                boolean returnIntermediate = scanner.nextBoolean();
-
+                System.out.print("Return intermediate arrays? (y/n): ");
+                char temp = scanner.next().charAt(0);
+                while(temp != 'y' && temp != 'n')
+                {
+                    System.out.print("\u001B[31mPlease enter valid option (y/n): \u001B[0m");
+                    temp = scanner.next().charAt(0);
+                }
+                boolean returnIntermediate ;
+                if(temp == 'y')
+                 returnIntermediate = true;
+                else
+                 returnIntermediate = false; 
+                long TimeElapsed; 
+                long start ;
+                long finish;
                 int[][] result;
                 switch (choice) {
                     case 1:
+                        start = System.nanoTime();
                         result = sortArray.simpleSort(returnIntermediate);
+                        finish = System.nanoTime();
+                        TimeElapsed = (finish-start)/1000 ;
                         break;
                     case 2:
+                        start = System.nanoTime();
                         result = sortArray.efficientSort(returnIntermediate);
+                        finish = System.nanoTime();
+                        TimeElapsed = (finish-start)/1000 ;
                         break;
                     case 3:
+                        start = System.nanoTime();
                         result = sortArray.nonComparisonSort(returnIntermediate);
+                        finish = System.nanoTime();
+                        TimeElapsed = (finish-start)/1000 ;
                         break;
                     default:
                         System.out.println("Invalid choice!");
@@ -51,6 +79,8 @@ public class Main {
                 for (int[] arr : result) {
                     System.out.println(Arrays.toString(arr));
                 }
+                System.out.println("\u001B[34mTime Taken = " + TimeElapsed + " µs\u001B[0m");
+                System.out.println("============================");
             }
         } catch (FileNotFoundException e) {
             System.out.println("File not found!");
@@ -59,10 +89,12 @@ public class Main {
 
     private static int[] readArrayFromFile(String filePath) throws FileNotFoundException {
         Scanner scanner = new Scanner(new File(filePath));
-        String[] elements = scanner.nextLine().split(",");
+        String line = scanner.nextLine().trim();
+        line = line.substring(1,line.length()-1);  //ignore two brackets
+        String[] elements = line.split(",");            //ignore commas
         int[] array = new int[elements.length];
         for (int i = 0; i < elements.length; i++) {
-            array[i] = Integer.parseInt(elements[i]);
+            array[i] = Integer.parseInt(elements[i].trim()); //ignore spaces
         }
         scanner.close();
         return array;
